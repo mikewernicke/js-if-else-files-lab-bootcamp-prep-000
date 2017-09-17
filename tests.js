4a46ad1093693b846fbfe41350b71e3592c43655
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-17 21:47:07 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -3,13 +3,13 @@ var index = require("./index.js")
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected: Susan, Received: "+index.name)
+    console.log(index.name)
 }
 
 
 
 if (index.height === 70) {
-    console.log("The height is correct")
+    console.log(index.height)
 } else {
     console.log("Expected: 70, Received: "+index.height)
 }
