02eb25fc330dd7b563bfb6a31ce8bc3fd5525c17
Mike Wernicke <mike.wernicke@gmail.com>
2017-09-17 21:48:21 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,9 +1,9 @@
 var index = require("./index.js")
 
 if (index.name === "Susan") {
-    console.log("The name is correct")
-} else {
     console.log(index.name)
+} else {
+    console.log("Expected: Susan, Received: "+index.name)
 }
 
 
